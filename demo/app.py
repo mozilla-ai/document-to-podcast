86294814cd887819952ed97a521af6c4d5f00b9f
daavoo <daviddelaiglesiacastro@gmail.com>
2024-11-15 20:47:14 +0100

    Update demo

--- demo/app.py
+++ demo/app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.title("Blueprint Demo")
+from opennotebookllm.preprocessing.data_loaders import load_pdf
+from opennotebookllm.preprocessing.data_cleaners import clean_html, clean_pdf
+
+
+uploaded_file = st.file_uploader("Choose a file", type=["pdf", "html"])
+
+if uploaded_file is not None:
+    if uploaded_file.type == "text/html":
+        raw_text = uploaded_file.getvalue().decode("utf-8")
+        clean_text = clean_html(raw_text)
+    elif uploaded_file.type == "application/pdf":
+        raw_text = load_pdf(uploaded_file)
+        clean_text = clean_pdf(raw_text)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.title("Raw Text")
+        st.write(raw_text[:200])
+    with col2:
+        st.title("Cleaned Text")
+        st.write(clean_text[:200])
--- src/opennotebookllm/preprocessing/data_loaders.py
+++ src/opennotebookllm/preprocessing/data_loaders.py
@@ -1,14 +1,18 @@
+from io import BytesIO
+
 import PyPDF2
 import PyPDF2.errors
-
 from loguru import logger
 
 
-def load_pdf(pdf_file: str) -> str | None:
+def load_pdf(pdf_file: str | BytesIO) -> str | None:
     try:
-        with open(pdf_file, "rb") as file:
-            pdf_reader = PyPDF2.PdfReader(file)
-            return "\n".join(page.extract_text() for page in pdf_reader.pages)
+        if isinstance(pdf_file, str):
+            with open(pdf_file, "rb") as file:
+                pdf_reader = PyPDF2.PdfReader(file)
+        else:
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+        return "\n".join(page.extract_text() for page in pdf_reader.pages)
     except Exception as e:
         logger.exception(e)
         return None
