812974ad0cd0f8b63b4287b3dee75d1d89e58760
Kostis-S-Z <kostissz@pm.me>
2024-12-10 13:24:05 +0200

    Update docstring SpeakerConfig

--- src/document_to_podcast/podcast_maker/config.py
+++ src/document_to_podcast/podcast_maker/config.py
@@ -9,17 +9,19 @@ from pydantic import BaseModel, ConfigDict
 class SpeakerConfig(BaseModel):
     """
     Pydantic model that stores configuration of an individual speaker for the TTS model.
+
+     model: The actual model instance to be used for generation.
+     speaker_id: A string defining the speaker in order to have a consistent voice during podcast generation.
+     speaker_profile: This profile is defined based on the specific model family used (e.g. Parler uses natural language descriptions whereas Oute models use IDs)
+     tokenizer: Parler models also need a tokenizer. This is None in the case of Oute models.
     """
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     model: Union[ParlerTTSForConditionalGeneration, InterfaceGGUFClass]
     speaker_id: str
-    # ParlerTTS specific configuration
+    speaker_profile: Optional[str] = None
     tokenizer: Optional[PreTrainedTokenizerBase] = None
-    speaker_description: Optional[str] = (
-        None  # This description is used by the ParlerTTS model to configure the speaker profile
-    )
 
 
 class PodcastConfig(BaseModel):
