286a93c0ae8cdfa3bc5a441ebe13ff35fdecea0b
Kostis-S-Z <kostissz@pm.me>
2024-12-17 16:09:07 +0000

    [WIP] Add bark and parler multi support

--- src/document_to_podcast/inference/model_loaders.py
+++ src/document_to_podcast/inference/model_loaders.py
@@ -1,9 +1,15 @@
 from typing import Tuple
-
 from huggingface_hub import hf_hub_download
 from llama_cpp import Llama
 from outetts import GGUFModelConfig_v1, InterfaceGGUF
-from transformers import AutoTokenizer, PreTrainedModel, PreTrainedTokenizerBase
+from transformers import (
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizerBase,
+    AutoProcessor,
+    BarkModel,
+    BarkProcessor,
+)
 
 
 def load_llama_cpp_model(
@@ -34,56 +40,79 @@ def load_llama_cpp_model(
     return model
 
 
-def load_outetts_model(
-    model_id: str, language: str = "en", device: str = "cpu"
-) -> InterfaceGGUF:
+def load_outetts_model(model_id: str, language: str = "en") -> InterfaceGGUF:
     """
     Loads the given model_id using the OuteTTS interface. For more info: https://github.com/edwko/OuteTTS
 
     Examples:
-        >>> model = load_outetts_model("OuteAI/OuteTTS-0.1-350M-GGUF/OuteTTS-0.1-350M-FP16.gguf", "en", "cpu")
+        >>> model = load_outetts_model("OuteAI/OuteTTS-0.1-350M-GGUF/OuteTTS-0.1-350M-FP16.gguf", "en")
 
     Args:
         model_id (str): The model id to load.
             Format is expected to be `{org}/{repo}/{filename}`.
         language (str): Supported languages in 0.2-500M: en, zh, ja, ko.
-        device (str): The device to load the model on, such as "cuda:0" or "cpu".
-
     Returns:
         PreTrainedModel: The loaded model.
     """
-    n_layers_on_gpu = 0 if device == "cpu" else -1
     model_version = model_id.split("-")[1]
 
     org, repo, filename = model_id.split("/")
     local_path = hf_hub_download(repo_id=f"{org}/{repo}", filename=filename)
-    model_config = GGUFModelConfig_v1(
-        model_path=local_path, language=language, n_gpu_layers=n_layers_on_gpu
-    )
+    model_config = GGUFModelConfig_v1(model_path=local_path, language=language)
 
     return InterfaceGGUF(model_version=model_version, cfg=model_config)
 
 
-def load_parler_tts_model_and_tokenizer(
-    model_id: str, device: str = "cpu"
-) -> Tuple[PreTrainedModel, PreTrainedTokenizerBase]:
+def load_bark_tts(model_id: str) -> Tuple[BarkModel, BarkProcessor]:
     """
-    Loads the given model_id using parler_tts.from_pretrained. For more info: https://github.com/huggingface/parler-tts
+    Loads the given model_id and its required processor. For more info: https://github.com/suno-ai/bark
 
     Examples:
-        >>> model, tokenizer = load_parler_tts_model_and_tokenizer("parler-tts/parler-tts-mini-v1", "cpu")
+        >>> model, processor = load_bark_tts("suno/bark", "cpu")
 
     Args:
         model_id (str): The model id to load.
             Format is expected to be `{repo}/{filename}`.
-        device (str): The device to load the model on, such as "cuda:0" or "cpu".
+
+    Returns:
+        BarkModel: The loaded model.
+        BarkProcessor: The loaded model.
+    """
+
+    processor = AutoProcessor.from_pretrained(model_id)
+    model = BarkModel.from_pretrained(model_id)
+
+    return model, processor
+
+
+def load_parler_tts_model_and_tokenizer(
+    model_id: str,
+) -> Tuple[PreTrainedModel, PreTrainedTokenizerBase, PreTrainedTokenizerBase]:
+    """
+    Loads the given model_id using parler_tts.from_pretrained. For more info: https://github.com/huggingface/parler-tts
+
+    Examples:
+        >>> model, tokenizer, _ = load_parler_tts_model_and_tokenizer("parler-tts/parler-tts-mini-v1")
+
+    Args:
+        model_id (str): The model id to load.
+            Format is expected to be `{repo}/{filename}`.
 
     Returns:
         PreTrainedModel: The loaded model.
+        PreTrainedTokenizer: The loaded tokenizer for the input.
+        PreTrainedTokenizer: [Only for the multilingual models] The loaded tokenizer for the description.
     """
     from parler_tts import ParlerTTSForConditionalGeneration
 
-    model = ParlerTTSForConditionalGeneration.from_pretrained(model_id).to(device)
+    model = ParlerTTSForConditionalGeneration.from_pretrained(model_id)
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-    return model, tokenizer
+    description_tokenizer = (
+        AutoTokenizer.from_pretrained(model.config.text_encoder._name_or_path)
+        if model_id == "parler-tts/parler-tts-mini-multilingual-v1.1"
+        or model_id == "ai4bharat/indic-parler-tts"
+        else None
+    )
+
+    return model, tokenizer, description_tokenizer
--- src/document_to_podcast/inference/text_to_speech.py
+++ src/document_to_podcast/inference/text_to_speech.py
@@ -2,7 +2,12 @@ from typing import Union
 
 import numpy as np
 from outetts.version.v1.interface import InterfaceGGUF
-from transformers import PreTrainedTokenizerBase, PreTrainedModel
+from transformers import (
+    PreTrainedTokenizerBase,
+    PreTrainedModel,
+    BarkModel,
+    BarkProcessor,
+)
 
 
 def _text_to_speech_oute(
@@ -25,13 +30,28 @@ def _text_to_speech_oute(
     return output_as_np
 
 
+def _text_to_speech__bark(
+    input_test: str, model: BarkModel, processor: BarkProcessor, voice_profile: str
+):
+    inputs = processor(input_test, voice_preset=voice_profile)
+
+    generation = model.generate(**inputs)
+    waveform = generation.cpu().numpy().squeeze()
+
+    return waveform
+
+
 def _text_to_speech_parler(
     input_text: str,
     model: PreTrainedModel,
     tokenizer: PreTrainedTokenizerBase,
     voice_profile: str,
+    description_tokenizer: PreTrainedTokenizerBase = None,
 ) -> np.ndarray:
-    input_ids = tokenizer(voice_profile, return_tensors="pt").input_ids
+    if description_tokenizer:
+        input_ids = description_tokenizer(voice_profile, return_tensors="pt").input_ids
+    else:
+        input_ids = tokenizer(voice_profile, return_tensors="pt").input_ids
     prompt_input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
     generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
@@ -42,9 +62,10 @@ def _text_to_speech_parler(
 
 def text_to_speech(
     input_text: str,
-    model: Union[InterfaceGGUF, PreTrainedModel],
+    model: Union[InterfaceGGUF, PreTrainedModel, BarkModel],
     voice_profile: str,
-    tokenizer: PreTrainedTokenizerBase = None,
+    processor: Union[BarkProcessor, PreTrainedTokenizerBase] = None,
+    description_tokenizer: PreTrainedTokenizerBase = None,
 ) -> np.ndarray:
     """
     Generates a speech waveform from a text input using a pre-trained text-to-speech (TTS) model.
@@ -58,15 +79,24 @@ def text_to_speech(
         voice_profile (str): Depending on the selected TTS model it should either be
             - a pre-defined ID for the Oute models (e.g. "female_1")
             more info here https://github.com/edwko/OuteTTS/tree/main/outetts/version/v1/default_speakers
+            - a pre-defined ID for the Bark model (e.g. "v2/en_speaker_0")
+            more info here https://suno-ai.notion.site/8b8e8749ed514b0cbf3f699013548683?v=bc67cff786b04b50b3ceb756fd05f68c
             - a natural description of the voice profile using a pre-defined name for the Parler model (e.g. Laura's voice is calm)
             more info here https://github.com/huggingface/parler-tts?tab=readme-ov-file#-using-a-specific-speaker
-        tokenizer (PreTrainedTokenizerBase): [Only used for the Parler models!] The tokenizer used for tokenizing the text in order to send to the model.
+            for the multilingual model: https://huggingface.co/parler-tts/parler-tts-mini-multilingual-v1.1
+            for the indic model: https://huggingface.co/ai4bharat/indic-parler-tts
+        processor (BarkProcessor or PreTrainedTokenizerBase): [Only used for the Bark or Parler models!]
+            In bark models, this is an HF processor. In Parler models, this is a pretrained tokenizer.
     Returns:
         numpy array: The waveform of the speech as a 2D numpy array
     """
     if isinstance(model, InterfaceGGUF):
         return _text_to_speech_oute(input_text, model, voice_profile)
+    elif isinstance(model, BarkModel):
+        return _text_to_speech__bark(input_text, model, processor, voice_profile)
     elif isinstance(model, PreTrainedModel):
-        return _text_to_speech_parler(input_text, model, tokenizer, voice_profile)
+        return _text_to_speech_parler(
+            input_text, model, processor, voice_profile, description_tokenizer
+        )
     else:
-        raise NotImplementedError("Model not yet implemented for TTS")
+        raise NotImplementedError
