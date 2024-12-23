a00a92816025d966287c75f95040caa76d195450
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2024-12-23 10:43:52 +0100

    Update download_models.py

--- demo/download_models.py
+++ demo/download_models.py
@@ -7,9 +7,8 @@ from document_to_podcast.inference.model_loaders import (
     load_outetts_model,
 )
 
-"""
+
 load_llama_cpp_model(
     "allenai/OLMoE-1B-7B-0924-Instruct-GGUF/olmoe-1b-7b-0924-instruct-q8_0.gguf"
 )
-"""
 load_outetts_model("OuteAI/OuteTTS-0.2-500M-GGUF/OuteTTS-0.2-500M-FP16.gguf")
