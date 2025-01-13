2ffc347b706a0089f6aa37be94a1990d33c26896
Pratik <steveparmar6nov2011@gmail.com>
2025-01-13 23:07:23 +0530

    Update Streamlit app to use load_file and markdown_to_text

--- src/document_to_podcast/preprocessing/__init__.py
+++ src/document_to_podcast/preprocessing/__init__.py
@@ -1,20 +1,20 @@
-from .data_loaders import load_pdf, load_txt, load_docx, load_url
-from .data_cleaners import clean_with_regex, clean_html, clean_markdown
+from .data_loaders import load_pdf, load_txt, load_docx, load_file
+from .data_cleaners import clean_with_regex, clean_html, clean_markdown, markdown_to_text
 
 
 DATA_LOADERS = {
-    ".docx": load_docx,
+    ".docx": load_file,
     ".html": load_txt,
     ".md": load_txt,
-    ".pdf": load_pdf,
+    ".pdf": load_file,
     ".txt": load_txt,
     "url": load_url,
 }
 
 DATA_CLEANERS = {
-    ".docx": clean_with_regex,
+    ".docx": markdown_to_text,
     ".html": clean_html,
     ".md": clean_markdown,
-    ".pdf": clean_with_regex,
+    ".pdf": markdown_to_text,
     ".txt": clean_with_regex,
 }
