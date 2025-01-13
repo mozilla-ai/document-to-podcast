2b446f4ca034510491e26648d682356821b3fa58
Pratik <steveparmar6nov2011@gmail.com>
2025-01-13 23:06:16 +0530

    Update tests

--- tests/integration/test_data_load_and_clean.py
+++ tests/integration/test_data_load_and_clean.py
@@ -1,22 +1,17 @@
 from document_to_podcast.preprocessing.data_cleaners import (
-    clean_html,
-    clean_with_regex,
     clean_markdown,
+    markdown_to_text
 )
-from document_to_podcast.preprocessing.data_loaders import load_pdf, load_txt
+from document_to_podcast.preprocessing.data_loaders import load_pdf, load_txt, load_file
 
 
 def test_load_and_clean_pdf(example_data):
-    text = clean_with_regex(load_pdf(example_data / "Mozilla-Trustworthy_AI.pdf"))
+    text = markdown_to_text(load_file(str(example_data / "Mozilla-Trustworthy_AI.pdf")))
     assert text[:50] == "Creating Trustworthy AI a Mozilla white paper on c"
 
 
 def test_load_and_clean_html(example_data):
-    text = clean_html(
-        load_txt(
-            example_data / "introducing-mozilla-ai-investing-in-trustworthy-ai.html"
-        )
-    )
+    text = markdown_to_text(load_file(str(example_data / "introducing-mozilla-ai-investing-in-trustworthy-ai.html")))
     assert text[:50] == "Skip to content Mozilla Internet Culture Deep Dive"
 
 
