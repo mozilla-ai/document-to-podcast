f8c8c5bd5a3da201dfec3df0c7c61ce5b3ad87d4
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2025-01-13 14:11:23 +0100

    Update reboot_space.py

--- demo/reboot_space.py
+++ demo/reboot_space.py
@@ -3,8 +3,6 @@ import os
 from huggingface_hub import HfApi
 
 if __name__ == "__main__":
-    repo_id = "Wauplin/my-cool-training-space"
-
     api = HfApi()
     api.restart_space(
         repo_id="mozilla-ai/document-to-podcast",
