c30d668b17ca2b1190073ae1e2a399e0b457abdd
Kostis-S-Z <kostissz@pm.me>
2025-02-06 10:14:10 -0300

    Update tests

--- tests/unit/inference/test_text_to_speech.py
+++ tests/unit/inference/test_text_to_speech.py
@@ -1,28 +1,28 @@
-from outetts.version.v1.interface import InterfaceGGUF
+from typing import Generator
+
+from kokoro import KPipeline
 
 from document_to_podcast.inference.model_loaders import TTSModel
 from document_to_podcast.inference.text_to_speech import text_to_speech
 
 
-def test_text_to_speech_oute(mocker):
-    model = mocker.MagicMock(spec_set=InterfaceGGUF)
+def test_text_to_speech_kokoro(mocker):
+    model = mocker.MagicMock(spec_set=KPipeline)
+    generator = mocker.MagicMock(spec_set=Generator)
     tts_model = TTSModel(
         model=model,
-        model_id="OuteAI/OuteTTS-0.1-350M-GGUF/OuteTTS-0.1-350M-FP16.gguf",
+        model_id="hexgrad/Kokoro-82M",
         sample_rate=0,
         custom_args={},
     )
     text_to_speech(
         input_text="Hello?",
         model=tts_model,
-        voice_profile="female_1",
+        voice_profile="af_sarah",
     )
 
-    model.load_default_speaker.assert_called_with(name=mocker.ANY)
-    model.generate.assert_called_with(
+    model.__call__.assert_called_with(
         text=mocker.ANY,
-        temperature=mocker.ANY,
-        repetition_penalty=mocker.ANY,
-        max_length=mocker.ANY,
-        speaker=mocker.ANY,
+        voice=mocker.ANY,
     )
+    generator.__next__.assert_called_with()
