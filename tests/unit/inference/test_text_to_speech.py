f98376e90593ed3dc58af813457bb6b67e48206a
Kostis-S-Z <kostissz@pm.me>
2025-02-06 10:26:30 -0300

    Update tests

--- tests/unit/inference/test_text_to_speech.py
+++ tests/unit/inference/test_text_to_speech.py
@@ -1,5 +1,3 @@
-from typing import Generator
-
 from kokoro import KPipeline
 
 from document_to_podcast.inference.model_loaders import TTSModel
@@ -8,7 +6,10 @@ from document_to_podcast.inference.text_to_speech import text_to_speech
 
 def test_text_to_speech_kokoro(mocker):
     model = mocker.MagicMock(spec_set=KPipeline)
-    generator = mocker.MagicMock(spec_set=Generator)
+    generator = mocker.MagicMock()
+    generator.__next__.return_value = (None, None, "mock_audio")
+    model.return_value = generator
+
     tts_model = TTSModel(
         model=model,
         model_id="hexgrad/Kokoro-82M",
@@ -21,8 +22,4 @@ def test_text_to_speech_kokoro(mocker):
         voice_profile="af_sarah",
     )
 
-    model.__call__.assert_called_with(
-        text=mocker.ANY,
-        voice=mocker.ANY,
-    )
-    generator.__next__.assert_called_with()
+    generator.__next__.assert_called_once()
